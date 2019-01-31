c39abbe32935e073cedb087be444621755b239ab
Jean-Philippe Braun <eon@patapon.info>
2019-01-31 16:55:45 +0100

    WIP

--- nixpkgs-fetch.nix
+++ nixpkgs-fetch.nix
@@ -6,6 +6,6 @@ in {
     owner = "NixOS";
     repo = "nixpkgs";
     # Belong to the branch release-18.09
-    rev = "7e88992a8c7b2de0bcb89182d8686b27bd93e46a";
-    sha256 = "1f6lf4addczi81hchqbzjlhrsmkrj575dmdjdhyl0jkm7ypy2lgk";};
+    rev = "ab118f384727637fa635d18268f00e8b6eface0e";
+    sha256 = "046qbx1wi93f3x99di4bdv8xdnh6j18c3w3809knx3h6qkykmgc2";};
   }
